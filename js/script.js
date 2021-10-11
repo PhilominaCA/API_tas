010c071a8ac9a10f358c04fdbe1ef8dfd7dcde8c
PhilominaCA <35137186+PhilominaCA@users.noreply.github.com>
2021-10-11 14:46:39 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
-//calender fetch function
+// country dropdown values fetch
+
+const countList = async() => {
+    try {
+        const cnty = await fetch(`https://api.getfestivo.com/v2/countries?api_key=4c68d56701acff769b51316be9b56115`);
+        const countryJson = await cnty.json();
+        let cntryTag = document.getElementById("country");
+        for (let i = 0; i < (countryJson.length); i++) {
+            let optionTag = document.createElement("option");
+            optionTag.value = countryJson[i].codeAlpha2;
+            optionTag.innerHTML = countryJson[i].name;
+            cntryTag.appendChild(optionTag);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+countList();
+
 async function getChurchOc() {
-    let defaultDt = document.getElementById("occDate").value;
+
+    //get month and country value from user
+    var selectMonth =
+        (document.querySelector('#month')).value;
+    var selectCntry =
+        (document.querySelector('#country')).value;
 
     //check for null value
-    if (defaultDt == "") {
-        document.getElementById("cal_data_disp").innerHTML = `<h2>Please select a month & year!!!</h2>`;
+    if (selectCntry == "") {
+        document.getElementById("cal_data_disp").innerHTML = `<h2>Please select a Country !!!</h2>`;
+    } else if (selectMonth == "") {
+        document.getElementById("cal_data_disp").innerHTML = `<h2>Please select a month !!!</h2>`;
     } else {
-        let inputDt = new Date(defaultDt);
-        let year = inputDt.getFullYear();
-        let month = inputDt.getMonth();
-
         //fetch API data
         try {
-            const calData = await fetch(`https://api.getfestivo.com/v2/holidays?country=IN&year=2020&api_key=4c68d56701acff769b51316be9b56115`); //`http://calapi.inadiutorium.cz/api/v0/en/calendars/default/${year}/${month+1}`);
+
+            const calData = await fetch(`https://api.getfestivo.com/v2/holidays?country=${selectCntry}&year=2020&api_key=4c68d56701acff769b51316be9b56115`); //`http://calapi.inadiutorium.cz/api/v0/en/calendars/default/${year}/${month+1}`);
 
             //API data manipulation
             const calJson = await calData.json();
@@ -20,28 +42,54 @@ async function getChurchOc() {
 
             //clear the data before each reqeust
             calDataDisp.innerHTML = "";
+            let dateObj = {};
 
+            //Get selected month holiday dates
             for (let i = 0; i < (calJson.holidays).length; i++) {
-                var dateDiv = document.createElement("div");
-                dateDiv.id = "dateDiv";
-                var dateSpan = document.createElement("span");
-                dateSpan.id = "date";
-                dateSpan.innerHTML = calJson.holidays[i].date;
-                var occSpan = document.createElement("span");
-                occSpan.id = "occSpan";
-                dateDiv.appendChild(dateSpan);
-                dateDiv.appendChild(occSpan);
-                calDataDisp.appendChild(dateDiv);
-
-                // for (let j = 0; j < calJson[i].celebrations.length; j++) {
-                let ooccSpan = document.querySelectorAll("#occSpan");
-                let occDiv = document.createElement("div");
-                occDiv.innerHTML = calJson.holidays[i].name;
-                occDiv.style.background = "white";
-                occDiv.id = "occDiv";
-                ooccSpan[i].appendChild(occDiv);
-                // }
+                var calDate = new Date(calJson.holidays[i].date);
+                if (parseInt(calDate.getMonth()) == parseInt(selectMonth) - 1) {
+                    dateObj[calJson.holidays[i].date] = [];
+                }
             }
+
+            //check if any holidays available
+            if (Object.keys(dateObj).length == 0) {
+                document.getElementById("cal_data_disp").innerHTML = `<h2>Sorry! No Holidays in this month.. </h2>`;
+
+            } else {
+                //if available get holiday note
+                for (let j = 0; j < (calJson.holidays).length; j++) {
+                    try {
+                        (dateObj[calJson.holidays[j].date]).push(calJson.holidays[j].name);
+                    } catch (ex) {
+                        console.log(ex);
+                    }
+                }
+
+                //grid creation
+                for (let i of Object.keys(dateObj)) {
+                    var dateDiv = document.createElement("div");
+                    dateDiv.id = "dateDiv";
+                    var dateSpan = document.createElement("span");
+                    dateSpan.id = "date";
+                    dateSpan.innerHTML = i;
+                    var occSpan = document.createElement("span");
+                    occSpan.id = "occSpan";
+                    dateDiv.appendChild(dateSpan);
+                    dateDiv.appendChild(occSpan);
+                    calDataDisp.appendChild(dateDiv);
+
+                    for (let j = 0; j < dateObj[i].length; j++) {
+                        let ooccSpan = document.querySelectorAll("#occSpan");
+                        let occDiv = document.createElement("div");
+                        occDiv.innerHTML = dateObj[i][j];
+                        occDiv.style.background = "white";
+                        occDiv.id = "occDiv";
+                        ooccSpan[ooccSpan.length - 1].appendChild(occDiv);
+                    }
+                }
+            }
+
         }
 
         //Error Handle
